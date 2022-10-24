4d67de88edc26051708ffe58d0d7d8b111710fd0
davlago <davlago@ucm.es>
2022-10-24 10:29:57 +0200

    avances

--- src/myThreeJsScene.js
+++ src/myThreeJsScene.js
@@ -3,32 +3,53 @@ const container = document.getElementById("pruebaCubo");
 const scene = new THREE.Scene();
 const widthSize = 850;
 const heightSize = 650;
-const camera = new THREE.PerspectiveCamera( 75, widthSize/heightSize, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 100, widthSize/heightSize, 1, 100000 );
 
 camera.position.z = 15;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( widthSize,heightSize );
+renderer.setClearColor( 0x000000, 0.5);
 container.appendChild( renderer.domElement );
+//LUCES
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
 
+const light = new THREE.PointLight( 0xffffff, 1, 100 );
+light.position.set( 2, 10, 4 );
+light.castShadow = true; // default false
+
+light.shadow.mapSize.width = 512; // default
+light.shadow.mapSize.height = 512; // default
+light.shadow.camera.near = 0.5; // default
+light.shadow.camera.far = 500; // default
+scene.add( light );
+//const helper = new THREE.CameraHelper( light.shadow.camera );
+//scene.add( helper );
+
+
+//CUBO
 const geometryCube = new THREE.BoxGeometry( 3, 3, 3 );
-const materialCube = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const materialCube = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
 const cube = new THREE.Mesh( geometryCube, materialCube );
 cube.position.x = 1;
 cube.position.y = 1;
+cube.castShadow = true; //default is false
+cube.receiveShadow = false; //default
 scene.add( cube );
 
-const geometryCircle = new THREE.SphereGeometry( 2, 32, 16 );
-const materialCircle = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const sphere = new THREE.Mesh( geometryCircle, materialCircle );
-sphere.position.x = 1;
-sphere.position.y = 5;
+//ESFERA
+const sphereGeometry = new THREE.SphereGeometry( 2, 32, 16 );
+const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.castShadow = true; //default is false
+sphere.receiveShadow = false; //default
 scene.add( sphere );
 
-
+//ANIMACIONES
 let translateCube = 0.1;
 let rotationCube = 0.03;
-function animateCube() {
+function animateSphere() {
     requestAnimationFrame( animateCube );
 
     if(cube.position.x > 9 || cube.position.y > 9){
@@ -47,10 +68,8 @@ function animateCube() {
     renderer.render( scene, camera );
 };
 
-animateCube();
-
 let translateSphere = 0.1;
-function animateSphere() {
+function animateCube() {
     requestAnimationFrame( animateSphere );
     if(sphere.position.y > 9){
         translateSphere = -0.1;
@@ -63,4 +82,19 @@ function animateSphere() {
 };
 
 animateSphere();
+animateCube();
+
+
+//LINEAS
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+points.push( new THREE.Vector3( 15, 5, 0 ) );
+points.push( new THREE.Vector3( 10, 15, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
 
