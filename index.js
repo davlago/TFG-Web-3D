0e669b81f27969b488822f1314db2d7c84aeff82
davlago <davlago@ucm.es>
2022-11-05 16:52:55 +0100

    dsa

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer(function(req,res){
-    fs.readFile('./dist/index.html', function(error, data){
+    fs.readFile('./index.html', function(error, data){
         if(error){
             res.writeHead(404);
             res.write("Error: No file")
         }
         else{
             res.write(data)
+            res.write("Load")
         }
     })
 });
