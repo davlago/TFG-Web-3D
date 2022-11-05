23ecd81e4dbff8e403f1344abdb6c9f32a76cf42
davlago <davlago@ucm.es>
2022-11-05 15:32:03 +0100

    dsa

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const server = http.createServer(function(req,res){
+    fs.readFile('dist/index.html', function(error, data){
+        if(error){
+            res.writeHead(404);
+            res.write("Error: No file")
+        }
+        else{
+            res.write(data)
+        }
+    })
+});
+
+server.listen(port, function(error){
+    if(error){
+        console.log("Error ", error);
+    }
+    else{
+        console.log("Correct", port);
+    }
+
+})
