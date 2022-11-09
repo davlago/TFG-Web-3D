cb59df7d7a64af58d455519ce0918040ef8c2b8f
davlago <davlago@ucm.es>
2022-11-09 19:06:46 +0100

    cambios

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-
     res.sendFile(path.join(__dirname, '/index.html'));
 });
   app.use(express.static(__dirname + '/'));
