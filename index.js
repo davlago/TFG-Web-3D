17c645cc768d14fd6727b031a5d52ffce74af458
davlago <davlago@ucm.es>
2022-11-24 09:51:02 +0100

    version1

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const app = express();
 const path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './dist/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
-  app.use(express.static(__dirname + '/'));
-  app.listen(3000);
+app.use(express.static(__dirname + '/'));
+app.listen(3000);
